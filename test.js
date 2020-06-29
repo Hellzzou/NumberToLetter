cb1b29c7205ef706ce2e8d0e9ff77d20e987ab59
Piednoel <greg@MacBook-Pro-de-Piednoel.local>
2020-06-29 23:20:28 +0200

    Transforme des nombres en lettres

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 function numberInLetter(numberToTransform){
 
-	let unit = numberToTransform % 10;
-	let ten = ( numberToTransform % 100 - unit ) / 10;
-	let hundred = ( numberToTransform - ( ten * 10 ) - unit) / 100;
+	const unit = numberToTransform % 10;
+	const ten = ( numberToTransform % 100 - unit ) / 10;
+	const hundred = ( numberToTransform - ( ten * 10 ) - unit) / 100;
 	let numberTransformed = '';
 
 	if ( hundred == 0 && ten == 0){																									// Affiche les unités si les centaines et les dizaines sont nulles
@@ -15,32 +15,41 @@ function numberInLetter(numberToTransform){
 		numberTransformed += numberArray[ten][2];																					// Affiche la dizaine 
 
 		if (ten != 0 && ten != 1 && ten != 8 && ten != 9 && unit == 1) numberTransformed += '-et';									// Ajoute un 'et' si l'unité est 1 et que nous somme pas dans un cas spéciale donc se finissant par 11,12 ...
-		if ( ten == 1 || ten == 7 || ten == 9) numberTransformed += numberArray[unit][1];											// Affiche les dizaines spéciales 11,12,etc....
-		if ( unit >= 7 || ( ten != 1 && ten != 7 && ten != 9)) numberTransformed = numberTransformed + '-' + numberArray[unit][0];	// Affiche les unités normales donc si nous ne somme pas dans les cas speciaux d'au dessus
+
+		numberTransformed += unitInLetters(ten,unit);
+
 	}
 	return numberTransformed;
 }
 
-function hundredInLetters(number){
-	let hundred = '';
-	if ( number > 1) {
-		hundred += numberArray[number][0] + '-cent';
-	}else if (number == 1 ) {
-		hundred += 'cent';
+function hundredInLetters(hundred){
+	if ( hundred > 1) {
+		return numberArray[hundred][0] + '-cent';
+	}else if (hundred == 1 ) {
+		return 'cent';
 	}
-	return hundred;
+	return'';
 }
+
+function unitInLetters(ten,unit){
+	if ( ten == 1 || ten == 7 || ten == 9) {
+		return numberArray[unit][1];																				// Affiche les dizaines spéciales 11,12,72,etc....
+			} else {								
+		return '-' + numberArray[unit][0];																		// Affiche les unités normales donc si nous ne somme pas dans les cas speciaux d'au dessus
+	}
+}
+
 let numberToTransform = '' ;
-let numberArray = [['zéro','',''],					
+let numberArray = [['zéro','dix',''],					
 				   ['un','onze',''],							
 				   ['deux','douze','vingt'],							
 				   ['trois','treize','trente'],
 				   ['quatre','quatorze','quarante'],
 				   ['cinq','quinze','cinquante'],
 				   ['six','seize','soixante'],
-				   ['sept','dix-','soixante'],
-				   ['huit','dix-','quatre-vingt'],
-				   ['neuf','dix-','quatre-vingt']];
+				   ['sept','dix-sept','soixante-'],
+				   ['huit','dix-huit','quatre-vingt'],
+				   ['neuf','dix-neuf','quatre-vingt-']];
 do {
 	numberToTransform = parseInt(prompt('Entrez un  nombre entre 0 et 999'));
 }
